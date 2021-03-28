4de93c0d45a062c3bbdb3bef83cb77ee9974684b
Irenna Lumbuun <irennanicole1@gmail.com>
2021-03-28 15:45:25 -0700

    Styling font size, reorder tags, etc

--- src/app/projects/projects.service.ts
+++ src/app/projects/projects.service.ts
@@ -55,38 +55,46 @@ export class ProjectsService{
         })
     }
 
-    getTagColor(tag_id: string){
+    getTagColor(tag_id: string): [string, number]{
         const frontend = ["react", "angular", "html-css", "html/css"]; // yellow
-        const cloud = ["aws", "google-cloud"]; // blue
+        const cloud = ["aws", "google-cloud", "docker"]; // blue
         const backend = ["javascript", "node.js", "python", "flask"]; // red
         const database = ["firebase-database", "mongodb", "dynamodb"]; // green
         const ml = ["machine-learning", "keras", "scikit", "computer-vision"]; // white
         const mobile = ["swift", "react-native"]; // orange
-        const system = ["c", "docker"]; // gray
+        const system = ["c"]; // gray
 
-        let color = "";
+        let color: string = "";
+        let order: number = 0;
         if (frontend.includes(tag_id)) {
-            color = "#FDFD96"
-        } 
+            color = "#FDFD96";
+            order = 2;
+        }
         else if (cloud.includes(tag_id)) {
             color = "#ABDEE6"
-        } 
+            order = 4;
+        }
         else if (backend.includes(tag_id)) {
-            color = "#FF968A"
-        } 
+            color = "#FF968A";
+            order = 1;
+        }
         else if (database.includes(tag_id)) {
-            color = "#D7ECD9"
-        } 
+            color = "#D7ECD9";
+            order = 3;
+        }
         else if (ml.includes(tag_id)) {
-            color = "#CDB7F6"
-        } 
+            color = "#CDB7F6";
+            order = 5;
+        }
         else if (mobile.includes(tag_id)) {
             color = "#F5D5CB"
-        } 
+            order = 6;
+        }
         else if (system.includes(tag_id)) {
             color = "#d3d3d3"
-        } 
-        return color;
+            order = 7;
+        }
+        return [color, order];
     }
 
     getTags(){
@@ -99,22 +107,22 @@ export class ProjectsService{
                     // manually remove some tags
                     // python and javascript is redundant
                     // so is keras and scikit
-                    // for now, google-cloud only includes cloud vision project, 
+                    // for now, google-cloud only includes cloud vision project,
                     // which are the same as the projects in "Computer Vision"
-                    if (d.id === "python" || d.id === "javascript" || d.id === "keras" || d.id==="scikit" || d.id ==="google-cloud") 
+                    if (d.id === "python" || d.id === "javascript" || d.id === "keras" || d.id==="scikit" || d.id ==="google-cloud")
                         continue;
 
-                    let color = this.getTagColor(d.id);
+                    let color_order = this.getTagColor(d.id)
                     tags.push({
                         "id": d.id,
                         "name": d.name,
-                        "color": color
+                        "color": color_order[0],
+                        "order": color_order[1]
                     });
 
                     // "sort" by color so it doesn't look messy in the front end side
                     tags.sort(function(tag1, tag2){
-                        return (tag1.color.charCodeAt(1) * tag1.color.charCodeAt(2)) - 
-                        (tag2.color.charCodeAt(1) * tag2.color.charCodeAt(2))
+                        return tag1.order - tag2.order;
                     })
                 }
                 resolve(tags);
