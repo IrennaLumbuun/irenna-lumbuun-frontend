04fef59cf912b5a9646c45cf3baba04d9a3ea6b5
Irenna Lumbuun <irennanicole1@gmail.com>
2021-03-28 22:18:35 -0700

    Irenna Lumbuun codes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+
+  async ngOnInit(){
+    let i = 0;
+    let speed = 50;
+    let txt = 'Irenna Lumbuun codes.';
+    let tagline = document.getElementById("tagline");
+    while (i < txt.length){
+      tagline.innerHTML += txt.charAt(i);
+      i++;
+      await this.sleep(speed)
+    }
+  }
+
+  sleep(ms:number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
 }
